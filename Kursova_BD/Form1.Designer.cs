3d9003e283c8d3ceb831b44836760f3d1bc2ea8c
ShevaTrasnik <max.shevchune@gmail.com>
2026-01-06 03:10:43 +0200

    Добавьте файлы проекта.

---
+++ Kursova_BD/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Kursova_BD
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridProducts = new System.Windows.Forms.DataGridView();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnEditProduct = new System.Windows.Forms.Button();
+            this.btnDeleteProduct = new System.Windows.Forms.Button();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnViewRecipe = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridProducts
+            // 
+            this.dataGridProducts.AllowUserToAddRows = false;
+            this.dataGridProducts.AllowUserToDeleteRows = false;
+            this.dataGridProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProducts.Location = new System.Drawing.Point(13, 50);
+            this.dataGridProducts.MultiSelect = false;
+            this.dataGridProducts.Name = "dataGridProducts";
+            this.dataGridProducts.ReadOnly = true;
+            this.dataGridProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridProducts.Size = new System.Drawing.Size(657, 252);
+            this.dataGridProducts.TabIndex = 0;
+            this.dataGridProducts.UseWaitCursor = true;
+            this.dataGridProducts.SelectionChanged += new System.EventHandler(this.dataGridProducts_SelectionChanged_1);
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(13, 13);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(154, 23);
+            this.btnAddProduct.TabIndex = 1;
+            this.btnAddProduct.Text = "Додати";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            this.btnAddProduct.UseWaitCursor = true;
+            this.btnAddProduct.Click += new System.EventHandler(this.btnAddProduct_Click);
+            // 
+            // btnEditProduct
+            // 
+            this.btnEditProduct.Location = new System.Drawing.Point(190, 13);
+            this.btnEditProduct.Name = "btnEditProduct";
+            this.btnEditProduct.Size = new System.Drawing.Size(154, 23);
+            this.btnEditProduct.TabIndex = 2;
+            this.btnEditProduct.Text = "Редагувати";
+            this.btnEditProduct.UseVisualStyleBackColor = true;
+            this.btnEditProduct.UseWaitCursor = true;
+            this.btnEditProduct.Click += new System.EventHandler(this.btnEditProduct_Click);
+            // 
+            // btnDeleteProduct
+            // 
+            this.btnDeleteProduct.Location = new System.Drawing.Point(371, 13);
+            this.btnDeleteProduct.Name = "btnDeleteProduct";
+            this.btnDeleteProduct.Size = new System.Drawing.Size(154, 23);
+            this.btnDeleteProduct.TabIndex = 3;
+            this.btnDeleteProduct.Text = "Видалити";
+            this.btnDeleteProduct.UseVisualStyleBackColor = true;
+            this.btnDeleteProduct.UseWaitCursor = true;
+            this.btnDeleteProduct.Click += new System.EventHandler(this.btnDeleteProduct_Click);
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(13, 333);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.ReadOnly = true;
+            this.txtProductName.Size = new System.Drawing.Size(154, 20);
+            this.txtProductName.TabIndex = 4;
+            this.txtProductName.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Назва";
+            this.label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Категорія";
+            this.label2.UseWaitCursor = true;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(203, 333);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.ReadOnly = true;
+            this.txtCategory.Size = new System.Drawing.Size(154, 20);
+            this.txtCategory.TabIndex = 7;
+            this.txtCategory.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 409);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Вага";
+            this.label3.UseWaitCursor = true;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(12, 425);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.ReadOnly = true;
+            this.txtWeight.Size = new System.Drawing.Size(154, 20);
+            this.txtWeight.TabIndex = 9;
+            this.txtWeight.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(200, 409);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Одиниця";
+            this.label4.UseWaitCursor = true;
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Location = new System.Drawing.Point(203, 425);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.ReadOnly = true;
+            this.txtUnit.Size = new System.Drawing.Size(154, 20);
+            this.txtUnit.TabIndex = 11;
+            this.txtUnit.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(419, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Опис продукту";
+            this.label5.UseWaitCursor = true;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(422, 333);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ReadOnly = true;
+            this.txtDescription.Size = new System.Drawing.Size(173, 65);
+            this.txtDescription.TabIndex = 13;
+            this.txtDescription.UseWaitCursor = true;
+            // 
+            // btnViewRecipe
+            // 
+            this.btnViewRecipe.Location = new System.Drawing.Point(422, 421);
+            this.btnViewRecipe.Name = "btnViewRecipe";
+            this.btnViewRecipe.Size = new System.Drawing.Size(173, 23);
+            this.btnViewRecipe.TabIndex = 14;
+            this.btnViewRecipe.Text = "Як готувати";
+            this.btnViewRecipe.UseVisualStyleBackColor = true;
+            this.btnViewRecipe.UseWaitCursor = true;
+            this.btnViewRecipe.Click += new System.EventHandler(this.btnViewRecipe_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 471);
+            this.Controls.Add(this.btnViewRecipe);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUnit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.btnDeleteProduct);
+            this.Controls.Add(this.btnEditProduct);
+            this.Controls.Add(this.btnAddProduct);
+            this.Controls.Add(this.dataGridProducts);
+            this.Name = "MainForm";
+            this.Text = "Головна";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridProducts;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.Button btnEditProduct;
+        private System.Windows.Forms.Button btnDeleteProduct;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnViewRecipe;
+    }
+}
+
---
+++ Kursova_BD/Form1.cs
@@ -0,0 +1,116 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Kursova_BD
+{
+    public partial class MainForm : Form
+    {
+        private readonly string _cs =
+    "Server=127.0.0.1;Port=3306;Database=bakery_db_clean;User Id=root;Password=VFRcbvrf2007;SslMode=Preferred;AllowPublicKeyRetrieval=True;";
+        private MySqlConnection conn;
+        private MySqlDataAdapter _adapter;
+        private DataTable _table;
+        public MainForm()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(_cs);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.UseWaitCursor = false;
+            Cursor.Current = Cursors.Default;
+            Application.UseWaitCursor = false;
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            try
+            {
+                string sql = @"
+            SELECT 
+                p.product_id,
+                p.name AS product_name,
+                c.name AS category_name,
+                p.weight,
+                p.unit,
+                p.description,
+                p.created_at
+            FROM bakery_products p
+            JOIN categories c ON p.category_id = c.category_id
+            ORDER BY p.name;
+        ";
+
+                using (var conn = new MySqlConnection(_cs))
+                {
+                    conn.Open();
+
+                    _adapter = new MySqlDataAdapter(sql, conn);
+                    _table = new DataTable();
+                    _adapter.Fill(_table);
+                }
+
+                dataGridProducts.DataSource = _table;
+
+                dataGridProducts.Columns["product_id"].Visible = false;
+                dataGridProducts.Columns["description"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка завантаження продуктів");
+            }
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeleteProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewRecipe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridProducts_SelectionChanged_1(object sender, EventArgs e)
+        {
+            if (dataGridProducts.CurrentRow == null)
+                return;
+
+            txtProductName.Text =
+                dataGridProducts.CurrentRow.Cells["product_name"].Value?.ToString() ?? "";
+
+            txtCategory.Text =
+                dataGridProducts.CurrentRow.Cells["category_name"].Value?.ToString() ?? "";
+
+            txtWeight.Text =
+                dataGridProducts.CurrentRow.Cells["weight"].Value?.ToString() ?? "";
+
+            txtUnit.Text =
+                dataGridProducts.CurrentRow.Cells["unit"].Value?.ToString() ?? "";
+
+            txtDescription.Text =
+                dataGridProducts.CurrentRow.Cells["description"].Value?.ToString() ?? "";
+        }
+    }
+}
